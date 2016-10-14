a2969a0467dc3ff13e9a2b18e1ceb04993ae98cd
dave <dave1@vt.edu>
2016-10-14 12:24:57 -0400

    work please.

--- js/globe_setup.js
+++ js/globe_setup.js
@@ -1,11 +1,33 @@
 if(!Detector.webgl){
       Detector.addGetWebGLMessage();
     } else {
-      var container = document.getElementById('globe_container');
+ var years = ['1990','1995','2000'];
+      var container = document.getElementById('container');
       var globe = new DAT.Globe(container);
+      console.log(globe);
       var i, tweens = [];
+      
+      var settime = function(globe, t) {
+        return function() {
+          new TWEEN.Tween(globe).to({time: t/years.length},500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+          var y = document.getElementById('year'+years[t]);
+          if (y.getAttribute('class') === 'year active') {
+            return;
+          }
+          var yy = document.getElementsByClassName('year');
+          for(i=0; i<yy.length; i++) {
+            yy[i].setAttribute('class','year');
+          }
+          y.setAttribute('class', 'year active');
+        };
+      };
+      
+      for(var i = 0; i<years.length; i++) {
+        var y = document.getElementById('year'+years[i]);
+        y.addEventListener('mouseover', settime(globe,i), false);
+      }
+      
       var xhr;
-
       TWEEN.start();
       
       
