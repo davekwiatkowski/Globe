5875b0f981b6faf61389c235f97215c69bb11dca
dave <dave1@vt.edu>
2016-10-14 12:29:06 -0400

    please.

--- js/globe_setup.js
+++ js/globe_setup.js
@@ -2,7 +2,7 @@ if (!Detector.webgl) {
   Detector.addGetWebGLMessage();
 } else {
   var years = ['1990', '1995', '2000'];
-  var container = document.getElementById('container');
+  var container = document.getElementById('globe_container');
   var globe = new DAT.Globe(container);
   console.log(globe);
   var i, tweens = [];
