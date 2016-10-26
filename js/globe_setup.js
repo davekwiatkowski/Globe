56f1d5ca64e1d5f41b7537817b4e28564284b850
dave <dave1@vt.edu>
2016-10-26 12:32:28 -0400

    new data

--- js/globe_setup.js
+++ js/globe_setup.js
@@ -1,48 +1,56 @@
-if (!Detector.webgl) {
-  Detector.addGetWebGLMessage();
-} else {
-  var years = ['1990', '1995', '2000'];
-  var container = document.getElementById('globe_container');
-  var globe = new DAT.Globe(container);
-  console.log(globe);
+if (!Detector.webgl) Detector.addGetWebGLMessage();
+else {
+  var modes = ['users', 'user_delta', 'swl'];
+  var globeContainer = document.getElementById('globe_container');
+  var globe = new DAT.Globe(globeContainer);
   var i, tweens = [];
 
-  var settime = function (globe, t) {
+  var setMode = function (globe, t) {
     return function () {
-      new TWEEN.Tween(globe).to({ time: t / years.length }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-      var y = document.getElementById('year' + years[t]);
-      if (y.getAttribute('class') === 'year active') {
-        return;
-      }
-      var yy = document.getElementsByClassName('year');
-      for (i = 0; i < yy.length; i++) {
-        yy[i].setAttribute('class', 'year');
-      }
-      y.setAttribute('class', 'year active');
+      // create the globe
+      new TWEEN.Tween(globe).to({ time: t / modes.length }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+
+      // get the current mode 
+      var mode = document.getElementById('mode_' + modes[t]);
+
+      // leave if mode is already active
+      if (mode.getAttribute('class') === 'mode active') return;
+
+      // get the mode elements
+      var modeElements = document.getElementsByClassName('mode');
+
+      // initialize the modes' activity to non-active
+      for (i = 0; i < modeElements.length; i++)
+        modeElements[i].setAttribute('class', 'mode');
+
+      // set the activity of the current mode to active  
+      mode.setAttribute('class', 'mode active');
     };
   };
 
-  for (var i = 0; i < years.length; i++) {
-    var y = document.getElementById('year' + years[i]);
-    y.addEventListener('mouseover', settime(globe, i), false);
+  // add the event listeners for the mode elements
+  for (var i = 0; i < modes.length; i++) {
+    var modeToChange = document.getElementById('mode_' + modes[i]);
+    modeToChange.addEventListener('mouseover', setMode(globe, i), false);
   }
 
-  var xhr;
   TWEEN.start();
 
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'globe_data.json', true);
 
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', 'population909500.json', true);
   xhr.onreadystatechange = function (e) {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         var data = JSON.parse(xhr.responseText);
         window.data = data;
-        for (i = 0; i < data.length; i++) {
+
+        for (i in data) {
           globe.addData(data[i][1], { format: 'magnitude', name: data[i][0], animated: true });
         }
+
         globe.createPoints();
-        settime(globe, 0)();
+        setMode(globe, 0)();
         globe.animate();
         document.body.style.backgroundImage = 'none'; // remove loading
       }
